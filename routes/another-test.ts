8c41b535600bdc23f8501967072db95f194403d2
Lance <andreas.frederiksen@proton.me>
2025-10-23 02:46:17 +0200

    feat: add post generation to user page (not working just a test)

--- routes/another-test.ts
+++ routes/another-test.ts
@@ -1,6 +1,16 @@
 import { html } from "../lib/html";
 
 export function render_user_page(user: { name: string; email: string }) {
+  function output_posts() {
+    let num: number = 0;
+    const posts: string[] = [];
+    while (num <= 4) {
+      num++;
+      posts.push(html`<p>${num}</p>`);
+    }
+    return posts.join("\n");
+  }
+
   return html`
     <!DOCTYPE html>
     <html>
@@ -11,6 +21,7 @@ export function render_user_page(user: { name: string; email: string }) {
         <h1>Welcome ${user.name}</h1>
         <p>your email is: ${user.email}</p>
         <p>Current time: ${new Date()}</p>
+        ${output_posts()}
       </body>
     </html>
   `;
